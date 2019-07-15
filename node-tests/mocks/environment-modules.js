90655b482ea24d9473eeb048ebad95f41a7474b3
Kus Cámara <kcmr@users.noreply.github.com>
2019-07-15 14:52:10 +0200

    test(integration): change config in fixtures

--- node-tests/mocks/environment-modules.js
+++ node-tests/mocks/environment-modules.js
@@ -16,6 +16,7 @@ module.exports = function(environment) {
 		},
 		'ember-cli-webcomponents-bundler': {
 			modules: true,
+			importStyles: true,
 			entrypointPaths: [
 				path.join(entrypoint, 'web-components')
 			]
